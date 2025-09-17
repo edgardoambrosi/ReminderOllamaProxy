5eb3ab54ca6700b73e47f4218659105c688f6232
Edgardo Ambrosi <edgardoambrosi@alia-space.com>
2025-09-17 11:01:44 +0200

    UPDATE

--- index.js
+++ index.js
@@ -4,11 +4,24 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 
-app.use(cors()); // permette tutte le origini
+var corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 300 
+  }
+app.use(cors(corsOptions));
+
+// Middleware per aggiungere l'header a tutte le risposte
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // o puoi mettere un dominio specifico
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
 app.use('/', createProxyMiddleware({
-  target: 'https://nupsi.alia-space.com:11435',
-  changeOrigin: true,
-  secure: false
-}));
+    target: 'https://nupsi.alia-space.com:11435',
+    changeOrigin: true,
+    secure: false
+})
+);
 
 app.listen(11433, () => console.log('Proxy attivo su http://localhost:11433'));
